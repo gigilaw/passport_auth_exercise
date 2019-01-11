10987a6fd760be0917f22cb9a0159c8a402902f2
gigilaw <gigi.law.14@gmail.com>
2019-01-11 12:59:57 +0800

    Passport setup

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 let express = require("express");
 let mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/auth_demo");
+let passport = require("passport");
+let bodyParser = require("body-parser");
+let localStrategy = require("passport-local");
+let passportLocalMongoose = require("passport-local-mongoose");
+let User = require("./models/user");
+
+mongoose.connect(
+  "mongodb://localhost/auth_demo",
+  { useNewUrlParser: true }
+);
 
 let app = express();
 app.set("view engine", "ejs");
+app.use(
+  require("express-session")({
+    secret: "shhhhhhh secret",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home");
